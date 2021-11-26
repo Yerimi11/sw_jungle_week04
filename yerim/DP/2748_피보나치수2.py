bd00f9e3530b442ec0293d8b4e1606688697f6b8
yerim <sontt4266@naver.com>
2021-11-26 22:39:29 +0900

    .

--- yerim/DP/2748_피보나치수2.py
+++ yerim/DP/2748_피보나치수2.py
@@ -2,9 +2,9 @@ import sys
 n = int(sys.stdin.readline())
 
 # DP = Memoization
-memoization = [0] * (n+1)
-memoization[1] = 1
+dp = [0] * (n+1)
+dp[1] = 1
 for i in range(2, n+1):
-    memoization[i] = memoization[i-2] + memoization[i-1]
+    dp[i] = dp[i-2] + dp[i-1]
 
-print(memoization[n])
+print(dp[n])
---
+++ yerim/DP/9084_동전.py
@@ -0,0 +1,9 @@
+import sys
+input(sys.stdin.readline)
+
+testcase = int(input())
+for i in range(testcase):
+    howmany_coin = int(input())
+    howmuch_coin = map(int, input().split())
+    amount = int(input())
+
